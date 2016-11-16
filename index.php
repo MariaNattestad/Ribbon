aaeb9cf4271440e15ddae8db79417d1511243e89
Maria Nattestad <maria.nattestad@gmail.com>
2016-11-15 17:02:42 -0800

    Showing 30 bedpe variants instead of 15 in table and noting for variants and features how many are shown in table

--- index.php
+++ index.php
@@ -639,7 +639,7 @@
 			<div class="panel-collapse collapse in" id="collapsible_variant_table_box">
 				<div class="panel-body">
 					<div id="variant_table_box">
-						<p> Sort by clicking column names. For bam files, click on a row in the table to fetch reads around that feature. </p>
+						<p> Showing only the first 30 variants. Sort by clicking column names. For bam files, click on a row in the table to fetch reads around that feature. </p>
 						<div id="variant_table_landing">
 							<!-- superTable creates a table here out of _Variants -->
 						</div>
@@ -658,7 +658,7 @@
 			<div class="panel-collapse collapse in" id="collapsible_bedpe_table_box">
 				<div class="panel-body">
 					<div id="bedpe_table_box">
-						<p> Sort by clicking column names, and filter by typing into the text boxes for each column. For instance, type >20 or =chr2. Separate multiple filters in a single column using spaces. For bam files, click on a row in the table to fetch reads around that feature. </p>
+						<p> Showing only the first 30 variants. Sort by clicking column names, and filter by typing into the text boxes for each column. For instance, type >20 or =chr2. Separate multiple filters in a single column using spaces. For bam files, click on a row in the table to fetch reads around that feature. </p>
 						<div id="bedpe_table_landing">
 							<!-- superTable creates a table here out of _Bedpe -->
 						</div>
@@ -701,7 +701,7 @@
 			<div class="panel-collapse collapse in" id="collapsible_feature_table_box">
 				<div class="panel-body">
 					<div id="feature_table_box">
-						<p> Sort by clicking column names. Filter by typing queries into text input boxes. </p>
+						<p> Showing only the first 15 features. Sort by clicking column names. Filter by typing queries into text input boxes. </p>
 						<div id="feature_table_landing">
 							
 						</div>
