37a5a3ddc51089c3bb40ffa26541d022f5377b2c
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2020-03-14 17:35:19 -0700

    Add script to manually create a compressed permalink

---
+++ js/permalink.js
@@ -0,0 +1,26 @@
+// Script to manually create permalinks
+
+const fs = require('fs');
+const pako = require('pako');
+
+if(process.argv.length < 4) {
+    console.log('Usage: node permalink.js ../path/to/json "Permalink Description"');
+    return;
+}
+
+// User input
+let fp = process.argv[2],
+    name = process.argv[3];
+
+// Output compressed JSON
+post_data = fs.readFileSync(fp, "utf8");
+console.log(
+    JSON.stringify({
+        name: name,
+        ribbon: Buffer.from(
+            pako.deflate(
+                JSON.stringify(post_data)
+            )
+        ).toString('base64')
+    })
+)
