0401774a67495b1635b66dccd50a609ff2f530bc
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2022-04-28 20:53:27 -0700

    Add comment

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# This script avoids using a bundler :)
 # Generate CSS and JS bundles. Note that we use `awk 1` instead of `cat` so we
 # append an extra line after each file. Otherwise, this could break if one of
 # the modules doesn't have a break line at the end of the min.js files.
