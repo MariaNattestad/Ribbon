3a0ea5cf2f2d5ff8b8d0befd7b4dc68125d19487
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2024-08-15 13:04:44 -0700

    Simplify bundler

--- bin/build.sh
+++ bin/build.sh
@@ -1,7 +1,31 @@
 #!/bin/bash
-
 # Hacky bundler script to generate CSS and JS bundles.
-# Note that we use `awk 1` instead of `cat` so we append an extra line after each file. Otherwise,
-# this could break if one of the modules doesn't have a break line at the end of the min.js files.
-awk 1 node_modules/{bootstrap/dist/css/bootstrap.min.css,jquery-ui-dist/jquery-ui.min.css} > css/bundle.css
-awk 1 node_modules/{d3/d3.min.js,jquery/dist/jquery.min.js,bootstrap/dist/js/bootstrap.min.js,@biowasm/aioli/dist/aioli.js,pako/dist/pako.min.js,moment/min/moment.min.js,jquery-ui-dist/jquery-ui.min.js,papaparse/papaparse.min.js} > js/bundle.js
+
+OUTPUT_CSS="css/bundle.css"
+OUTPUT_JS="js/bundle.js"
+
+CSS_MODULES=(
+    "bootstrap/dist/css/bootstrap.min.css"
+    "jquery-ui-dist/jquery-ui.min.css"
+)
+
+JS_MODULES=(
+    "d3/d3.min.js"
+    "jquery/dist/jquery.min.js"
+    "bootstrap/dist/js/bootstrap.min.js"
+    "@biowasm/aioli/dist/aioli.js"
+    "pako/dist/pako.min.js"
+    "moment/min/moment.min.js"
+    "jquery-ui-dist/jquery-ui.min.js"
+    "papaparse/papaparse.min.js"
+)
+
+echo -n > "$OUTPUT_CSS"
+for file in "${CSS_MODULES[@]}"; do
+    cat "node_modules/$file" <(echo) >> "$OUTPUT_CSS"
+done
+
+echo -n > "$OUTPUT_JS"
+for file in "${JS_MODULES[@]}"; do
+    cat "node_modules/$file" <(echo) >> "$OUTPUT_JS"
+done
