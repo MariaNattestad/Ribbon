e23e15460e4080aba887ff5bb4885ea3a3b96ad6
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2024-08-15 10:48:22 -0700

    Replace download() with more modern API

--- js/index_ribbon.js
+++ js/index_ribbon.js
@@ -6758,22 +6758,13 @@ function screenshot_individual_reads() {
   _index_within_read_index_list = 0;
   load_next_read();
 }
-// Thanks to http://stackoverflow.com/questions/2897619/using-html5-javascript-to-generate-and-save-a-file
-function download(filename, text) {
-  var pom = document.createElement("a");
-  pom.setAttribute(
-    "href",
-    "data:text/plain;charset=utf-8," + encodeURIComponent(text)
-  );
-  pom.setAttribute("download", filename);
 
-  if (document.createEvent) {
-    var event = document.createEvent("MouseEvents");
-    event.initEvent("click", true, true);
-    pom.dispatchEvent(event);
-  } else {
-    pom.click();
-  }
+function download(filename, text) {
+  const blob = new Blob([text]);
+  const link = document.createElement("a");
+  link.href = URL.createObjectURL(blob);
+  link.download = filename;
+  link.click();
 }
 
 function create_and_download_info(num_split) {
