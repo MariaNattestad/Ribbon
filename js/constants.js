9a54a8a2105f9c29b0787d78c56df9840c86a378
Maria Nattestad <maria.nattestad@gmail.com>
2024-09-13 10:28:33 -0700

    Update example sessions with various HG008 datasets

--- js/constants.js
+++ js/constants.js
@@ -1,14 +1,44 @@
 export const EXAMPLE_SESSIONS = [
   {
-    name: "giab_hg008",
-    description: "Revio HG008 Tumor/Normal",
+    name: "giab_hg008_gripss",
+    description: "GIAB HG008 HiFi normal/tumor with gripss calls",
     bam: [
-      "https://42basepairs.com/download/s3/giab/data_somatic/HG008/Liss_lab/PacBio_Revio_20240125/HG008-T_PacBio-HiFi-Revio_20240125_116x_GRCh38-GIABv3.bam",
-      "https://42basepairs.com/download/s3/giab/data_somatic/HG008/Liss_lab/PacBio_Revio_20240125/HG008-N-P_PacBio-HiFi-Revio_20240125_35x_GRCh38-GIABv3.bam"
+      "https://42basepairs.com/download/s3/giab/data_somatic/HG008/Liss_lab/BCM_Revio_20240313/HG008-N-D_PacBio-HiFi-Revio_20240313_68x_GRCh38-GIABv3.bam",
+      "https://42basepairs.com/download/s3/giab/data_somatic/HG008/Liss_lab/BCM_Revio_20240313/HG008-T_PacBio-HiFi-Revio_20240313_106x_GRCh38-GIABv3.bam",
+    ],
+    vcf: [
+      "https://ftp-trace.ncbi.nlm.nih.gov/ReferenceSamples/giab/data_somatic/HG008/Liss_lab/analysis/NCBI_Illumina_WGS_BCM_Somatic_SVs_GRIDSS2_Passage23_20240510/tumor.gripss.filtered.sv.annotated_Illumina_Passage23_GRCh38.vcf",
+    ],
+    coverage:
+      "https://42basepairs.com/download/s3/giab-data/ribbon-json/splitthreader_HG008-T_BCMIllumina_195x.regions.bed",
+    annotation_id: "GRCh38",
+  },
+  {
+    name: "giab_hg008_dragen",
+    description: "GIAB HG008 Tumor/Normal with DRAGEN calls",
+    bam: [
+      "https://42basepairs.com/download/s3/giab/data_somatic/HG008/Liss_lab/BCM_Revio_20240313/HG008-N-D_PacBio-HiFi-Revio_20240313_68x_GRCh38-GIABv3.bam",
+      "https://42basepairs.com/download/s3/giab/data_somatic/HG008/Liss_lab/BCM_Revio_20240313/HG008-T_PacBio-HiFi-Revio_20240313_106x_GRCh38-GIABv3.bam",
     ],
     vcf: [
       "https://42basepairs.com/download/s3/giab/data_somatic/HG008/Liss_lab/analysis/DRAGEN-v4.2.4_ILMN-WGS_20240312/standard/dragen_4.2.4_HG008-mosaic_tumor.sv.vcf.gz",
     ],
+    coverage:
+      "https://42basepairs.com/download/s3/giab-data/ribbon-json/splitthreader_HG008-T_BCMIllumina_195x.regions.bed",
+    annotation_id: "GRCh38",
+  },
+  {
+    name: "giab_hg008_minda",
+    description: "GIAB HG008 normal/tumor with Minda ensemble calls",
+    bam: [
+      "https://42basepairs.com/download/s3/giab/data_somatic/HG008/Liss_lab/BCM_Revio_20240313/HG008-N-D_PacBio-HiFi-Revio_20240313_68x_GRCh38-GIABv3.bam",
+      "https://42basepairs.com/download/s3/giab/data_somatic/HG008/Liss_lab/BCM_Revio_20240313/HG008-T_PacBio-HiFi-Revio_20240313_106x_GRCh38-GIABv3.bam",
+    ],
+    bedpe: [
+      "https://42basepairs.com/download/s3/giab-data/ribbon-json/HG008_minda_ensemble.vcf.bed",
+    ],
+    coverage:
+      "https://42basepairs.com/download/s3/giab-data/ribbon-json/splitthreader_HG008-T_BCMIllumina_195x.regions.bed",
     annotation_id: "GRCh38",
   },
   {
@@ -18,16 +48,6 @@ export const EXAMPLE_SESSIONS = [
     coverage: "/resources/examples/skbr3.coverage.bed",
     annotation_id: "hg19",
   },
-  // {
-  //   name: "bedpe without coverage",
-  //   bedpe: "/resources/examples/skbr3.bedpe.csv",
-  //   // coverage: "/resources/examples/skbr3.coverage.bed",
-  // },
-  // {
-  //   name: "coverage without bedpe",
-  //   // bedpe: "/resources/examples/skbr3.bedpe.csv",
-  //   coverage: "/resources/examples/skbr3.coverage.bed",
-  // },
   // {
   //   name: "kitchen_sink", // for testing
   //   bam: [
